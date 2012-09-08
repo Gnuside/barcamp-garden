a16da474bec4a11d43de0f2101b5be9c4115658e
Glenn Y. Rolland <glenux@glenux.net>
2012-09-08 21:15:20 +0200

    Inital import (rails new barcamp-garden).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarcampGarden::Application.config.secret_token = '1b4af1e632a6ac71dd3cebb2ec201e39022913d314ddba87b9d80104ff19039e44ebc93d9dafc3becff657b4482f1e9412b7de1a015692635724c00b49192b58'
