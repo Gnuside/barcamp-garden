c1a20cafe3422bfb9529826077d372927bd8ecd9
Glenn Y. Rolland <glenux@glenux.net>
2012-09-10 20:12:13 +0200

    Added omniauth separate config support.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
+OMNIAUTH_CONFIG = YAML.load_file(
+	File.join( RAILS_ROOT, "config", "omniauth.yml")
+)
+
 Rails.application.config.middleware.use OmniAuth::Builder do  
-	provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'  
-	provider :facebook, 'APP_ID', 'APP_SECRET'  
+	provider :twitter, OMNIAUTH_CONFIG[:twitter][:consumer_key], OMNIAUTH_CONFIG[:twitter][:consumer_secret]
+	provider :facebook, OMNIAUTH_CONFIG[:facebook][:app_id], OMNIAUTH_CONFIG[:facebook][:app_secret]
 	#provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'  
 end  
