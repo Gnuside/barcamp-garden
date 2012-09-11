75ccdf4bbfdfce308c001868ee6313471cf2a4da
Glenn Y. Rolland <glenux@glenux.net>
2012-09-11 21:50:23 +0200

    slots: improve db model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
   attr_accessible :date_end, :date_start, :description, :slug, :title
+
+  has_many :slots, :dependent => :destroy
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,3 +1,5 @@
 class Slot < ActiveRecord::Base
   attr_accessible :dtend, :dtstart, :event
+
+  belongs_to :event
 end
