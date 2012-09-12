8894e41c7e970b7e7b6ed0faf7d658cb821970ae
Glenn Y. Rolland <glenux@glenux.net>
2012-09-12 12:15:32 +0200

    initializers/omniauth: improve config loading & errors.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,20 @@
-OMNIAUTH_CONFIG = YAML.load_file(
-	Rails.root.join( 'config', 'omniauth.yml')
-)
 
+OMNIAUTH_CONFIG_PATH = Rails.root.join( 'config', 'omniauth.yml')
+
+if not File.exist? OMNIAUTH_CONFIG_PATH then
+	STDERR.puts "ERROR: #{OMNIAUTH_CONFIG_PATH} is missing. ",
+		"Please set up omniauth configuration, refering to #{OMNIAUTH_CONFIG_PATH}.example"
+	exit 1
+end
+
+OMNIAUTH_CONFIG = YAML.load_file( OMNIAUTH_CONFIG_PATH )
+
+# FIXME: validate presence of fields and load providers accordingly.
+
+=begin
 require 'pp'
 pp OMNIAUTH_CONFIG
+=end
 
 Rails.application.config.middleware.use OmniAuth::Builder do  
 	provider :twitter, OMNIAUTH_CONFIG[:twitter][:consumer_key], OMNIAUTH_CONFIG[:twitter][:consumer_secret]
