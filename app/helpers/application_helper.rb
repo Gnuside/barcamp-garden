51a854dfe99d354f714b653e3e83a6d44bad8c84
Glenn Y. Rolland <glenux@glenux.net>
2012-09-19 23:46:06 +0200

    application_helper: reduce gravatar size.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def avatar_url(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-		"http://gravatar.com/avatar/#{gravatar_id}.png"
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=48x48"
 	end
 end
