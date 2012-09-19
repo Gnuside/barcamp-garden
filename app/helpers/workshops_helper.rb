b9df8cf20d80547791962562b3986f27c64fe702
Glenn Y. Rolland <glenux@glenux.net>
2012-09-19 23:43:51 +0200

    Improved workshop display.

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -1,5 +1,15 @@
 module WorkshopsHelper
+	def event_workshop_pad_name event, workshop
+		"#{event.slug}_#{workshop.slug}"
+	end
+
 	def event_workshop_pad_url event, workshop
-		File.join(ETHERPAD_URL,'p',"#{event.slug}_#{workshop.slug}")
+		username = if current_user then
+					   current_user.email
+				   else 'anonymous'
+				   end
+		File.join(ETHERPAD_URL,'p',
+				  event_workshop_pad_name(event,workshop) + 
+				 "?showControls=false&useMonospaceFont=true&userName=#{username}")
 	end
 end
