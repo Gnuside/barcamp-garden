e2e6fb64f5160b212a24491c2bc6cefd41490a8b
Glenn Y. Rolland <glenux@glenux.net>
2012-09-28 18:17:57 +0200

    Improve migration for usernames.

--- db/migrate/20120923220259_add_name_to_user.rb
+++ db/migrate/20120923220259_add_name_to_user.rb
@@ -1,9 +1,25 @@
 class AddNameToUser < ActiveRecord::Migration
-  def change
+  def up
 	  add_column :users, :firstname, :string
-	  change_column :users, :firstname, :string, :null=>false
-	  add_column :users, :lastname, :string, :null=>false	
-	  change_column :users, :lastname, :string, :null=>false
+	  add_column :users, :lastname, :string
 	  add_column :users, :company, :string
+	  for user in User.all
+		  if user.firstname.nil? then
+			  user.update_attribute(:firstname, 'Anonymous')
+			  puts "Updated user #{user.id} with firstname #{user.firstname}"
+		  end
+		  if user.lastname.nil? then
+			  user.update_attribute(:lastname, 'Anonymous')
+			  puts "Updated user #{user.id} with lastname #{user.lastname}"
+		  end
+	  end
+	  change_column :users, :firstname, :string, :null=>false
+	  change_column :users, :lastname, :string, :null=>false
+  end
+
+  def down
+	  remove_column :users, :firstname, :string
+	  remove_column :users, :lastname, :string, :null=>false	
+	  remove_column :users, :company, :string
   end
 end
